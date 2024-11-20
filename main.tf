c7608db6dd69e1f731851be4718126485029377c
GonzaloROD <rodriguezcabrera.gonzalo@gmail.com>
2024-11-20 17:29:04 -0300

    Adding Backend s3

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-##Create and bootstrap webserver #lanzar
+# Configuración del Backend para almacenar el estado en S3
+terraform {
+  backend "s3" {
+    bucket = "mi-bucket-terraform-state-12345"   # Nombre del bucket S3
+    key    = "terraform.tfstate"                 # Nombre del archivo de estado dentro del bucket
+    region = "us-east-1"                         # Región de AWS donde se encuentra el bucket S3
+    encrypt = true                              # Activar cifrado para el archivo de estado
+    acl     = "bucket-owner-full-control"        # Permite control total al propietario del bucket
+  }
+}
+
+# Crear y lanzar una instancia webserver en AWS
 resource "aws_instance" "webserver" {
-  ami                         = "ami-0866a3c8686eaeeba"
-  instance_type               = "t2.micro"
+  ami                         = "ami-0866a3c8686eaeeba"   # ID de la AMI
+  instance_type               = "t2.micro"                # Tipo de instancia EC2
   #key_name                    = app-ssh-key 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
